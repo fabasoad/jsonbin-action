5ef1b5fd5fe299264c13fa4eb2408995dce1684c
fabasoad <fabasoad@gmail.com>
2022-02-13 19:54:09 +0900

    Bump snyk from 1.780.0 to 1.852.0

--- __tests__/test-jsonbin.js
+++ __tests__/test-jsonbin.js
@@ -13,7 +13,7 @@ const getBin = async (url) => {
 describe('Test JSONbin client', () => {
   const binToDelete = [];
 
-  it('CRUD works as expected', async () => {
+  it('CRUD works as expected', async (done) => {
     const expected = {
       'test': 1,
       'value': [3.2, 32.1, 0.01]
@@ -32,11 +32,13 @@ describe('Test JSONbin client', () => {
     );
     const { record: actual2 } = await getBin(resp2.url);
     expect(expected).to.deep.equal(actual2);
+    done();
   });
 
-  after(async () => {
+  after(async (done) => {
     await Promise.all(
       binToDelete.map((id) => jsonbin.delete(process.env.API_KEY, id))
     );
+    done();
   });
 });
