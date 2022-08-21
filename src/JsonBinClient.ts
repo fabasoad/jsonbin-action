73205098358e5cee0e29153731284c70700205a0
fabasoad <fabasoad@gmail.com>
2022-08-21 10:58:11 +0900

    Migrate from JS to TS

--- src/JsonBinClient.ts
+++ src/JsonBinClient.ts
@@ -1,72 +1,115 @@
 import { RestClient } from 'typed-rest-client/RestClient'
 
+export class JsonBinResponseError extends Error {
+  constructor(statusCode: number, message: string) {
+    super(`[${statusCode}] ${message}`)
+  }
+}
+
+export class JsonBinEmptyResponseError extends JsonBinResponseError {
+  constructor(statusCode: number) {
+    super(statusCode, 'Response is empty')
+  }
+}
+
+type GetResponse<T> = {
+  message?: string,
+  record: T,
+  metadata: {
+    id: string,
+    createdAt: string,
+    private: boolean
+  }
+}
+
+type CreateResponse<T> = {
+  message?: string,
+  record: T,
+  metadata: {
+    id: string,
+    createdAt: string,
+    private: boolean
+  }
+}
+
+type ReplaceResponse<T> = {
+  message?: string,
+  record: T,
+  metadata: {
+    private: boolean,
+    parentId: string
+  }
+}
+
+type DeleteResponse = {
+  message: string,
+  metadata: {
+    id: string,
+    versionDeleted: number
+  }
+}
+
 export interface JsonBinResponse {
   url: string,
   id: string
 }
 
-export class JsonBinEmptyResponseError extends Error {}
-
-type Response<T> = {
-  record: T,
-  metadata: {
-    id: string,
-    createdAt: string,
-    private: boolean,
-    parentId?: string
-  }
-}
-
 export default class JsonBinClient {
   private readonly URL: string = 'https://api.jsonbin.io/v3/b/'
-
   private readonly client: RestClient
-  private readonly apiKey: string
 
   constructor(apiKey: string) {
-    this.apiKey = apiKey;
-    this.client = new RestClient(null, this.URL)
+    this.client = new RestClient(this.constructor.name, this.URL, [], {
+      headers: {
+        'X-Master-Key': apiKey
+      }
+    })
+  }
+
+  public async get<T>(binId: string): Promise<T> {
+    const resp = await this.client.get<GetResponse<T>>(binId)
+    if (resp.result == null) {
+      throw new JsonBinEmptyResponseError(resp.statusCode)
+    } else if (resp.result?.message != null) {
+      throw new JsonBinResponseError(resp.statusCode, resp.result.message)
+    }
+    return resp.result.record as T
   }
 
   public async create<T>(body: T): Promise<JsonBinResponse> {
-    const { result } = await this.client.create<Response<T>>(this.URL, JSON.stringify(body), {
-      additionalHeaders: {
-        'X-Master-Key': this.apiKey,
-        'Content-Type': 'application/json'
-      }
-    })
-    if (result == null) {
-      throw new JsonBinEmptyResponseError()
+    const resp = await this.client.create<CreateResponse<T>>('', body)
+    if (resp.result == null) {
+      throw new JsonBinEmptyResponseError(resp.statusCode)
+    } else if (resp.result?.message != null) {
+      throw new JsonBinResponseError(resp.statusCode, resp.result.message)
     }
-    const binId: string = result.metadata.id
+    const binId: string = resp.result.metadata.id || 'unknown'
     return { id: binId, url: this.URL + binId }
   }
 
   public async update<T>(binId: string, body: T): Promise<JsonBinResponse> {
-    const { result } = await this.client.update<Response<T>>(this.URL, body, {
+    const resp = await this.client.replace<ReplaceResponse<T>>(binId, body, {
       additionalHeaders: {
-        'X-Master-Key': this.apiKey,
-        'Content-Type': 'application/json',
         'X-Bin-Versioning': false
       }
     })
-    if (result == null) {
-      throw new JsonBinEmptyResponseError()
+    if (resp.result == null) {
+      throw new JsonBinEmptyResponseError(resp.statusCode)
+    } else if (resp.result?.message != null) {
+      throw new JsonBinResponseError(resp.statusCode, resp.result.message)
     }
-    const parentBinId: string = result.metadata.parentId || 'unknown'
+    const parentBinId: string = resp.result.metadata.parentId || 'unknown'
     return { id: parentBinId, url: this.URL + parentBinId }
   }
 
   public async delete(binId: string): Promise<JsonBinResponse> {
-    const { result } = await this.client.del<Response<any>>(this.URL + binId, {
-      additionalHeaders: {
-        'X-Master-Key': this.apiKey
-      }
-    })
-    if (result == null) {
-      throw new JsonBinEmptyResponseError()
+    const resp = await this.client.del<DeleteResponse>(binId)
+    if (resp.result == null) {
+      throw new JsonBinEmptyResponseError(resp.statusCode)
+    } else if (resp.result?.message != null) {
+      throw new JsonBinResponseError(resp.statusCode, resp.result.message)
     }
-    const respBinId: string = result.metadata.id
+    const respBinId: string = resp.result.metadata.id || 'unknown'
     return { id: respBinId, url: this.URL + respBinId }
   }
 }
--- src/__tests__/JsonBinClient.spec.ts
+++ src/__tests__/JsonBinClient.spec.ts
@@ -1,46 +1,30 @@
 import 'dotenv/config'
-import { RestClient } from 'typed-rest-client/RestClient'
 import JsonBinClient from '../JsonBinClient'
-
-type Response<T> = {
-  metadata: {
-    id: string,
-    parentId: string
-  },
-  record: T
-}
-
-async function getBin<T>(url: string): Promise<Response<T>> {
-  const restClient: RestClient = new RestClient(null, url)
-  const { result } = await restClient.get<Response<T>>(
-    url, { additionalHeaders: { 'X-Master-Key': process.env.API_KEY } })
-  return result as Response<T>
-}
 
 describe('Test JSONbin client', () => {
   const binToDelete: string[] = []
-  const jsonBinClient = new JsonBinClient(process.env.API_KEY || '')
+  const client = new JsonBinClient(process.env.API_KEY || '')
 
-  it('CRUD works as expected', async () => {
+  test('CRUD works as expected', async () => {
     const expected = {
       'test': 1,
       'value': [3.2, 32.1, 0.01]
     }
-    const resp1 = await jsonBinClient.create<typeof expected>(expected)
+    const resp1 = await client.create<typeof expected>(expected)
     binToDelete.push(resp1.id)
-    const { record: actual1 } = await getBin<typeof expected>(resp1.url)
-    expect(expected).toMatchObject(actual1)
+    let actual = await client.get<typeof expected>(resp1.id)
+    expect(expected).toMatchObject(actual)
 
     expected['test'] += 2
     expected['value'].push(5.05)
-    const resp2 = await jsonBinClient.update<typeof expected>(resp1.id, expected)
-    const { record: actual2 } = await getBin<typeof expected>(resp2.url)
-    expect(expected).toMatchObject(actual2)
+    const resp2 = await client.update<typeof expected>(resp1.id, expected)
+    actual = await client.get<typeof expected>(resp2.id)
+    expect(expected).toMatchObject(actual)
   })
 
   afterAll(async () => {
     await Promise.all(
-      binToDelete.map((id) => jsonBinClient.delete(id))
+      binToDelete.map((id: string) => client.delete(id))
     )
   })
 })
