4c9749f175ef28c831e3089d1c3b9762f42e519c
fabasoad <fabasoad@gmail.com>
2022-08-21 11:45:07 +0900

    Debug functional tests

--- src/JsonBinClient.ts
+++ src/JsonBinClient.ts
@@ -72,9 +72,9 @@ export default class JsonBinClient {
 
   public async create<T>(body: T): Promise<JsonBinResponse> {
     const resp = await this.client.create<CreateResponse<T>>('', body)
-    // if (resp.result == null) {
-    //   throw new JsonBinEmptyResponseError(resp.statusCode)
-    // } else if (resp.result?.message != null) {
+    if (resp.result == null) {
+      throw new JsonBinEmptyResponseError(resp.statusCode)
+    } // else if (resp.result?.message != null) {
     //   throw new JsonBinResponseError(resp.statusCode, resp.result.message)
     // }
     // const binId: string = resp.result.metadata.id || 'unknown'
