6a25a4a2025131db412133d1f2747f156d2626c1
fabasoad <fabasoad@gmail.com>
2026-05-01 23:42:14 +0900

    chore: remove a.sh

--- a.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-jq -cn \
-  --arg mk "${JSONBIN_MASTER_KEY}" \
-  --arg ak "${JSONBIN_ACCESS_KEY}" \
-  '{"X-Master-Key": $mk, "X-Access-Key": $ak, "X-Bin-Meta": "true", "Accept": "application/json"}'
